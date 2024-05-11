b209b2782183937de86e19b4b83dbe83518cb560
Aaron (Home) <armahillo@gmail.com>
2024-05-11 10:52:04 -0400

    Adds validation for first name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # First Name is required
+  validates :first_name, presence: true
+  # Phone Number must be in the format: XXX.XXX.XXXX
+  # Email Address must be in a valid email format
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  let(:valid_user) { described_class.new(first_name: "Bob")}
+
+  describe "Validations >" do
+    it "is invalid without a first_name" do
+      expect {
+        valid_user.first_name = nil
+      }.to change { valid_user.valid? }.from(true).to(false)
+    end
+  end
+
 end
